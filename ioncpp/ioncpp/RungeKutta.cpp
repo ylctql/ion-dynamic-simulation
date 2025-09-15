5a5b0db2c0762a3075dc18fe2f15c37a6272fd44
ylcyyds <alexyan0431@gmail.com>
2025-09-15 10:42:47 +0800

    Replace RK4 with Velocity-Verlet

--- ioncpp/ioncpp/RungeKutta.cpp
+++ ioncpp/ioncpp/RungeKutta.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <chrono>
+#include <fstream>
+#include <filesystem>
 
 namespace ioncpp
 {
@@ -26,6 +28,7 @@ VecType CoulombInteraction(
 	Eigen::Matrix<data_t, 1, Eigen::Dynamic> ones_col = Eigen::Matrix<data_t, 1, Eigen::Dynamic>::Ones(1, N);
 	Eigen::Matrix<data_t, Eigen::Dynamic, 1> ones_row = Eigen::Matrix<data_t, Eigen::Dynamic, 1>::Ones(N, 1);
 
+	// Calculation of Coulomb interaction
 	Eigen::Matrix<data_t, Eigen::Dynamic, Eigen::Dynamic> dist2 = 
 		r.rowwise().squaredNorm().matrix() * ones_col
 		+ ones_row * r.rowwise().squaredNorm().transpose().matrix()
@@ -41,6 +44,8 @@ VecType CoulombInteraction(
 		).rowwise().sum();
 	}
 
+	// Finish Coulomb calculation
+
 	auto end = std::chrono::steady_clock::now();
 	elapsed1 += std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
 
@@ -48,6 +53,18 @@ VecType CoulombInteraction(
 }
 
 }
+
+void saveArray(const VecType& arr, const std::string& filename)
+	{
+		std::ofstream ofs(filename, std::ios::binary);
+		ofs.write(reinterpret_cast<const char*>(arr.data()), arr.size() * sizeof(data_t));
+	}
+
+void loadArray(VecType& arr, const std::string& filename)
+	{
+		std::ifstream ifs(filename, std::ios::binary);
+		ifs.read(reinterpret_cast<char*>(arr.data()), arr.size() * sizeof(data_t));
+	}
 
 std::pair<std::vector<VecType>, std::vector<VecType>> CalcTrajRK(
 	CRef<VecType>& init_r,
@@ -75,6 +92,8 @@ std::pair<std::vector<VecType>, std::vector<VecType>> CalcTrajRK(
 	VecType r = init_r;
 	VecType v = init_v;
 
+	//RK4
+	/*
 	VecType r_tmp(init_r.rows(), DIM);
 	VecType v_tmp(init_r.rows(), DIM);
 
@@ -87,11 +106,16 @@ std::pair<std::vector<VecType>, std::vector<VecType>> CalcTrajRK(
 	VecType v_k2(init_r.rows(), DIM);
 	VecType v_k3(init_r.rows(), DIM);
 	VecType v_k4(init_r.rows(), DIM);
+	*/
+	
+	VecType a_last(init_r.rows(), DIM);
 
 	for (size_t i = 0; i < step; i++)
 	{
 		double t = time_start + dt * (double)i;
 		
+		// RK4
+		/*
 		tmp = std::chrono::steady_clock::now();
 		v_k1 = (force(r, v, t)+ CoulombInteraction(r, charge)).colwise() / mass * dt;
 		elapsed2 += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - tmp);
@@ -119,6 +143,26 @@ std::pair<std::vector<VecType>, std::vector<VecType>> CalcTrajRK(
 		r_k4 = v_tmp * dt;
 		v += v_k1 / 6.0 + v_k2 / 3.0 + v_k3 / 3.0 + v_k4 / 6.0;
 		r += r_k1 / 6.0 + r_k2 / 3.0 + r_k3 / 3.0 + r_k4 / 6.0;
+		*/
+
+		// Velocity Verlet
+		tmp = std::chrono::steady_clock::now();
+		std::filesystem::path a_file = "../data_cache/a.bin";
+		if (!std::filesystem::exists(a_file))
+		{
+			a = (force(r, v, t) + CoulombInteraction(r, charge)).colwise() / mass;
+			saveArray(a, a_file.string());
+			r += v * dt + a * (dt * dt / 2.0);
+			v += a * dt;
+		}
+		else{
+			loadArray(a_last, a_file.string());
+			r += v * dt + a_last * (dt * dt / 2.0);
+			a = (force(r, v, t + dt) + CoulombInteraction(r, charge)).colwise() / mass;
+			saveArray(a, a_file.string());
+			v += (a + a_last) * (dt / 2.0);
+		}
+		elapsed2 += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - tmp);
 
 		r_ret.push_back(r);
 		v_ret.push_back(v);
